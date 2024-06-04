65903232713629ba61e4bf2536c6e1b7af054351
Samira Subía <subiakarensamira@gmail.com>
2024-06-04 11:43:02 -0500

    feat(main): JS repaired

--- src/index.js
+++ src/index.js
@@ -2,39 +2,38 @@ import analyzer from './analyzer.js';
 
 //TODO: escuchar eventos del DOM e invocar  los métodos del objeto `analyzer`
 
-const textArea = document.getElementById('userInput');
+const wordCount = document.getElementById('wordCount')
+const charCount = document.getElementById('charCount');
+const charCountNoSpaces = document.getElementById('charCountNoSpaces');
+const numberCount = document.getElementById('numberCount');
+const numberSum = document.getElementById('numberSum')
+const avgWordLength = document.getElementById('avgWordLength')
 
-textArea.addEventListener('input', function(){
-    
+const textArea = document.getElementById('userInput');
+textArea.addEventListener('input', function(){   
   console.log('entrar', textArea.value.length);
     
-  const resetButton = document.getElementById('resetButton');
-  textArea.value = '';
-  resetButton.addEventListener('click', () => {
-    wordCount.innerText = '0';
-    charCount.innerText = '0';
-    charCountNoSpaces.innerText = '0';
-    numberCount.innerText = '0';
-    numberSum.innerText = '0';
-    avgWordLength.innerText = '0';
-  });
-
-  const wordCount = document.getElementById('wordCount')
   wordCount.innerText = analyzer.getWordCount(textArea.value);
 
-  const charCount = document.getElementById('charCount');
   charCount.innerText = analyzer.getCharacterCount(textArea.value);
 
-  const charCountNoSpaces = document.getElementById('charCountNoSpaces');
   charCountNoSpaces.innerText = analyzer.getCharacterCountExcludingSpaces(textArea.value);
 
-  const numberCount = document.getElementById('numberCount');
   numberCount.innerText = analyzer.getNumberCount(textArea.value);
 
-  const numberSum = document.getElementById('numberSum')
   numberSum.innerText = analyzer.getNumberSum(textArea.value);
 
-  const avgWordLength = document.getElementById('avgWordLength')
   avgWordLength.innerText = analyzer.getAverageWordLength(textArea.value);
 
 })
+
+const resetButton = document.getElementById('resetButton');
+resetButton.addEventListener('click', () => {
+  textArea.value = '';
+  wordCount.innerText = '0';
+  charCount.innerText = '0';
+  charCountNoSpaces.innerText = '0';
+  numberCount.innerText = '0';
+  numberSum.innerText = '0';
+  avgWordLength.innerText = '0';
+});
